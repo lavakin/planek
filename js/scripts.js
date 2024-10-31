9ab795916d979ed7264a0d50e1d6b4e58bd604af
lavakin <nikola@kalabova.eu>
2024-10-31 20:03:22 +0100

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
+function showFloor(floorNumber) {
+    const floor1 = document.getElementById('floor1');
+    const floor2 = document.getElementById('floor2');
+
+    if (floorNumber === "1") {
+        floor1.style.display = 'inline-block';
+        floor2.style.display = 'none';
+    } else if (floorNumber === "2") {
+        floor1.style.display = 'none';
+        floor2.style.display = 'inline-block';
+    }
+}
+
 function showImage(imageSrc) {
-    document.getElementById('popupImage').src = imageSrc;
-    document.getElementById('imagePopup').style.display = 'block';
+    const popup = document.getElementById('imagePopup');
+    const popupImage = document.getElementById('popupImage');
+
+    if (popup && popupImage) {
+        popupImage.src = imageSrc;
+        popup.style.display = 'block';
+    }
 }
 
 function closePopup() {
-    document.getElementById('imagePopup').style.display = 'none';
+    const popup = document.getElementById('imagePopup');
+    if (popup) popup.style.display = 'none';
 }
-
